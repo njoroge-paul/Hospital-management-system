e8668d1a17d24dca4fc3399eeaddb95bed21620f
Derricknjaramba <njarambaderrick@gmail.com>
2024-10-16 11:45:29 +0300

    patientauth

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import AdminAuth from './components/AdminAuth'; // Import Admin Auth component
-//import PatientAuth from './components/PatientAuth'; // Import Patient Auth component
+import PatientAuth from './components/PatientAuth'; // Import Patient Auth component
 import DoctorAuth from './components/DoctorAuth'; // Import Doctor Auth component
 import AdminDashboard from './components/AdminDashboard';
 import DoctorDashboard from './components/DoctorDashboard';
@@ -18,7 +18,7 @@ function App() {
       <Routes> 
        <Route path="/" element={<LandingPage />} /> 
         <Route path="/admin/auth" element={<AdminAuth />} /> Route for Admin Auth
-        {/* <Route path="/patient/auth" element={<PatientAuth />} /> Route for Patient Auth */}
+        <Route path="/patient/auth" element={<PatientAuth />} /> Route for Patient Auth 
         <Route path="/doctor/auth" element={<DoctorAuth />} /> Route for Doctor Login
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/doctor" element={<DoctorDashboard />} />
