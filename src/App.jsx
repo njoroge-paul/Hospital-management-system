97ace7d8fbc0230f4418bd003306696bd0fd0207
Patrick <njorogepatrick739@gmail.com>
2024-10-16 10:54:48 +0300

    updated routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 //import LandingPage from './components/LandingPage';
 import AdminAuth from './components/AdminAuth'; // Import Admin Auth component
 //import PatientAuth from './components/PatientAuth'; // Import Patient Auth component
-//import DoctorAuth from './components/DoctorAuth'; // Import Doctor Auth component
+import DoctorAuth from './components/DoctorAuth'; // Import Doctor Auth component
 import AdminDashboard from './components/AdminDashboard';
-//import DoctorDashboard from './components/DoctorDashboard';
+import DoctorDashboard from './components/DoctorDashboard';
 //import PatientDashboard from './components/PatientDashboard';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -19,9 +19,9 @@ function App() {
        {/* <Route path="/" element={<LandingPage />} />  */}
         <Route path="/admin/auth" element={<AdminAuth />} /> Route for Admin Auth
         {/* <Route path="/patient/auth" element={<PatientAuth />} /> Route for Patient Auth */}
-        {/* <Route path="/doctor/auth" element={<DoctorAuth />} /> Route for Doctor Login */}
+        <Route path="/doctor/auth" element={<DoctorAuth />} /> Route for Doctor Login
         <Route path="/admin" element={<AdminDashboard />} />
-        {/* <Route path="/doctor" element={<DoctorDashboard />} /> */}
+        <Route path="/doctor" element={<DoctorDashboard />} />
         {/* <Route path="/patient" element={<PatientDashboard />} /> */}
       </Routes>
     </Router>
